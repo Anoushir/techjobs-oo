b5aa03af3cad6313feef0e7487214f1bd4cd2d2a
Anoushir Mansouri <anoushir.mansouri@gmail.com>
2017-12-13 18:54:36 -0600

    finished

--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -22,17 +22,56 @@ public class JobForm {
     @NotNull
     private int employerId;
 
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getPositionTypeId() {
+        return positionTypeId;
+    }
+
+    public void setPositionTypeId(int positionTypeId) {
+        this.positionTypeId = positionTypeId;
+    }
+
+    public int getCoreCompetencyId() {
+        return coreCompetencyId;
+    }
+
+    public void setCoreCompetencyId(int coreCompetencyId) {
+        this.coreCompetencyId = coreCompetencyId;
+    }
+
     /*
-        TODO #3 - Included other fields needed to create a job,
-        with correct validation attributes and display names.
-        Don't forget to add getters and setters
-     */
+            TODO #3 - Included other fields needed to create a job,
+            with correct validation attributes and display names.
+            Don't forget to add getters and setters
+         */
 
+    @NotNull
     private ArrayList<Employer> employers;
-    private ArrayList<Location> locations;
-    private ArrayList<CoreCompetency> coreCompetencies;
-    private ArrayList<PositionType> positionTypes;
 
+    @NotNull
+    private int locationId;
+
+    @NotNull
+    private ArrayList<Location> locations;
+
+    @NotNull
+    private int coreCompetencyId;
+
+    @NotNull
+    private ArrayList<CoreCompetency> coreCompetencies;
+
+    @NotNull
+    private int positionTypeId;
+
+    @NotNull
+    private ArrayList<PositionType> positionTypes;
     public JobForm() {
 
         JobData jobData = JobData.getInstance();
@@ -40,9 +79,10 @@ public class JobForm {
         /*
             TODO #4 - populate the other ArrayList collections needed in the view
         */
-
         employers = jobData.getEmployers().findAll();
-
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
     }
 
     public String getName() {
